019393292cff5cf884986749097c65c890a74487
otaylor2023 <otaylor2023@pingry.org>
2021-04-22 11:18:46 -0400

    created tensorflow class

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/Tensorflow.java
@@ -0,0 +1,55 @@
+package org.firstinspires.ftc.teamcode.testing;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.CameraDirection;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+
+// sourced from https://github.com/FIRST-Tech-Challenge/FtcRobotController/blob/master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptTensorFlowObjectDetection.java
+public class Tensorflow {
+    // tensorflow model settings
+    private static final String TFOD_MODEL_ASSET = "UltimateGoal.tflite";
+    private static final String LABEL_FIRST_ELEMENT = "Quad";
+    private static final String LABEL_SECOND_ELEMENT = "Single";
+
+    // make tensorflow object detector object var
+    private TFObjectDetector tfod;
+    private HardwareMap hardwareMap;
+    private VuforiaLocalizer vuforia;
+
+    public Tensorflow(HardwareMap hardwareMap, VuforiaLocalizer vuforia) {
+        this.vuforia = vuforia;
+        this.hardwareMap = hardwareMap;
+    }
+
+    public List<Recognition> getRecognitionList() {
+        List<Recognition> recognitionList = tfod.getUpdatedRecognitions();
+        return recognitionList;
+    }
+
+    public int getRecognitionCount() {
+        List<Recognition> recognitionList = tfod.getUpdatedRecognitions();
+        return recognitionList.size();
+    }
+    /**
+     * Initialize the TensorFlow Object Detection engine.
+     */
+    private void initTfod() {
+        int tfodMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
+                "tfodMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        TFObjectDetector.Parameters tfodParameters = new TFObjectDetector.Parameters(tfodMonitorViewId);
+        tfodParameters.minResultConfidence = 0.8f;
+        tfod = ClassFactory.getInstance().createTFObjectDetector(tfodParameters, vuforia);
+        tfod.loadModelFromAsset(TFOD_MODEL_ASSET, LABEL_FIRST_ELEMENT, LABEL_SECOND_ELEMENT);
+    }
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/TestOpMode.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.teamcode.testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name="Testing: Test opmode", group="Testing")
+public class TestOpMode extends OpMode {
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void init_loop() {
+    }
+
+    @Override
+    public void start() {
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+
+
+
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    @Override
+    public void stop() {
+    }
+}
