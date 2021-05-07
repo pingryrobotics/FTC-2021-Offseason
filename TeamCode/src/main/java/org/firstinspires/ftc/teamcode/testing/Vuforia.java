7fd06abe27486d86a20ebaf600530f9614007953
Alan Zhong <67126920+Tizariox@users.noreply.github.com>
2021-05-07 13:48:48 -0400

    changed vuforia initialization and methods to be the same as the opmode, added camera measurements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/Vuforia.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/testing/Vuforia.java
@@ -27,56 +27,67 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
  *  ^ comments in that file explain this a lot more in depth
  */
 public class Vuforia {
-    public static final String TAG = "Vuforia Localizer";
 
-    private VuforiaLocalizer vuforiaLocalizer;
-    private OpenGLMatrix lastLocation = null;
-    private HardwareMap hardwareMap;
-    private VuforiaLocalizer.CameraDirection cameraDirection;
+	public static final String TAG = "Vuforia Navigation";
 
-    private OpenGLMatrix phoneLocationOnRobot;
+	OpenGLMatrix lastLocation = null;
 
-    private Hashtable<TrackableItem, VuforiaTrackable> trackablesList;
+	VuforiaLocalizer vuforia;
 
-    // constants for robot/field measurements
-    private static final float mmPerInch        = 25.4f; // constant for mm to inches
-    private static final float inchesBotWidth = 18; // width of robot in inches. SET MANUALLY
-    private static final float mmBotWidth       = inchesBotWidth * mmPerInch; // width of robot to mm
+	// for capturing frame to file method
+	int captureCounter = 0;
+	File captureDirectory = AppUtil.ROBOT_DATA_DIR;
 
-    private static final float mmFTCFieldWidth  = (12*12 - 2) * mmPerInch; // width of field in mm. SET MANUALLY
+	// webcam
+	WebcamName webcamName;
 
-    /** BEGIN INITIALIZATIION */
+	// variables we'll use later for the webcam
+	private boolean targetVisible = false;
+	private float phoneXRotate = 0;
+	private float phoneYRotate = 0;
+	private float phoneZRotate = 0;
 
-    /**
-     * Initialize the vuforia localizer, trackable locations, and everything else
-     * @param hardwareMap the hardware map
-     */
+	// mm bc they're the recommended units and it's easier to use mm for calculations
+	private float mmPerInch = 25.4f;
+	private float mmBotWidth = (float) 17.7 * mmPerInch; // change the 17.7 to whatever the robot width is
+	private float mmTargetHeight = (6) * mmPerInch; // the height of the center of the target image above the floor
+
+	// Constants for perimeter targets
+	private float halfField = 72 * mmPerInch;
+	private float quadField = 36 * mmPerInch
+	
+	List<VuforiaTrackable> allTrackables = new ArrayList<VuforiaTrackable>();
+
     public Vuforia(HardwareMap hardwareMap) {
         this.hardwareMap = hardwareMap;
-        cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
         // initialize pure vuforia things
         initializeVuforia();
         setPhoneLocation();
         initializeTrackables();
-
-    }
+	}
 
     /**
      * Initialize the vuforia localizer object
      */
     public void initializeVuforia() {
-        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier(
-                "cameraMonitorViewId",
-                "id", hardwareMap.appContext.getPackageName());
+		// initialize camera and vuforia
+		webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
 
-        VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
+		int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+		VuforiaLocalizer.Parameters parameters = new VuforiaLocalizer.Parameters(cameraMonitorViewId);
 
-        parameters.vuforiaLicenseKey = "AVnPa5X/////AAABmUhfO30V7UvEiFRLKEAy25cwZ/uQDK2M0Z8GllUIhUOhFey2tkv1iKXqY4JdAjTHq4vlEUqn4F9sgeh+1ZiBsoPbGnSCdRnnHyQKmIU1hRoCyh24OvMfaG+6JQnpWlHorMoGWAqcEGt1+GXI9x3v2GLwooT1Dv/biDVn2DKar6tKms7EEEwIWkMN5YVaiQo53rbSSajpWuEROYYIrUrgzmgyorf4ngUWmjPrWHPES0OkUW6YVrZXoGT3Rwkiyl0Y7j5Rc5qT7iFBmI4v6E9udfPpnIsYrGzlhcL7GqHBntY8TuMYMTNIcklCO+ATWT4guojTwEOaNK+bVHG3XXxJsodhBK+Tbf7QX262rIbWvQto";
+		parameters.vuforiaLicenseKey = "AVnPa5X/////AAABmUhfO30V7UvEiFRLKEAy25cwZ/uQDK2M0Z8GllUIhUOhFey2tkv1iKXqY4JdAjTHq4vlEUqn4F9sgeh+1ZiBsoPbGnSCdRnnHyQKmIU1hRoCyh24OvMfaG+6JQnpWlHorMoGWAqcEGt1+GXI9x3v2GLwooT1Dv/biDVn2DKar6tKms7EEEwIWkMN5YVaiQo53rbSSajpWuEROYYIrUrgzmgyorf4ngUWmjPrWHPES0OkUW6YVrZXoGT3Rwkiyl0Y7j5Rc5qT7iFBmI4v6E9udfPpnIsYrGzlhcL7GqHBntY8TuMYMTNIcklCO+ATWT4guojTwEOaNK+bVHG3XXxJsodhBK+Tbf7QX262rIbWvQto";
+		parameters.cameraName = webcamName;
+		parameters.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
 
-        // set camera direction
-        parameters.cameraDirection = cameraDirection;
-        // instantiate the vuforia localizer
-        vuforiaLocalizer = ClassFactory.getInstance().createVuforia(parameters);
+		parameters.useExtendedTracking = false;
+
+		vuforia = ClassFactory.getInstance().createVuforia(parameters);
+		vuforia.enableConvertFrameToBitmap();
+
+		// for capturing frame to file method
+		AppUtil.getInstance().ensureDirectoryExists(captureDirectory);
+
     }
 
     /**
@@ -88,91 +99,92 @@ public class Vuforia {
      * Currently, this is just using the defaults from the source
      */
     public void initializeTrackables() {
-        trackablesList = new Hashtable<TrackableItem, VuforiaTrackable>();
+		// loading the trackables from xml file, replace with name of xml file in the current year
+		VuforiaTrackables UltimateGoal = this.vuforia.loadTrackablesFromAsset("UltimateGoal");
 
-        // load the StonesAndChips dataset
-        // custom datasets also use this, probably, but you load a different thing obviously
-        VuforiaTrackables stonesAndChips = vuforiaLocalizer.loadTrackablesFromAsset("StonesAndChips");
+		// these are the pictures that vuforia tracks to determine the robot location; change them out according to the current year's pictures
 
-        /**
-         * RED TARGET
-         * Position:
-         * Image: stones
-         */
-        // assign a name to each trackable in the dataset, add to the list, & set location
-        VuforiaTrackable redTarget = stonesAndChips.get(0);
-        trackablesList.put(TrackableItem.RED_TARGET, redTarget);
-        redTarget.setName("RedTarget");  // Stones
+		// Template
+		// VuforiaTrackable imageUse = xmlFileName.get(image index out of all images in xml file)
+		VuforiaTrackable blueTowerGoal = UltimateGoal.get(0);
+		VuforiaTrackable redTowerGoal = UltimateGoal.get(1);
+		VuforiaTrackable redWallTarget = UltimateGoal.get(2); 
+		VuforiaTrackable blueWallTarget = UltimateGoal.get(3);
+		VuforiaTrackable frontWall = UltimateGoal.get(4);
 
-        // set red location on field
-        // this is wrong btw, we need to set manually
-        // look at the github
-        OpenGLMatrix redTargetLocationOnField = OpenGLMatrix
-                .translation(-mmFTCFieldWidth/2, 0, 0)
-                .multiplied(Orientation.getRotationMatrix(
-                        AxesReference.EXTRINSIC, AxesOrder.XZX,
-                        AngleUnit.DEGREES, 90, 90, 0));
-        redTarget.setLocation(redTargetLocationOnField);
-        RobotLog.ii(TAG, "Red Target=%s", format(redTargetLocationOnField));
+		// set names
+		blueTowerGoal.setName("BlueTowerGoal");
+		redTowerGoal.setName("RedTowerGoal");
+		redWallTarget.setName("RedWallTarget");
+		blueWallTarget.setName("BlueWallTarget");
+		frontWall.setName("FrontWall");
 
-        // let red target listener know where phone is
-        ((VuforiaTrackableDefaultListener)redTarget.getListener()).setPhoneInformation(phoneLocationOnRobot, cameraDirection);
+		// adds all of the previously declared pictures, we can also do it manually for each picture
+		allTrackables.addAll(UltimateGoal);
 
-        /**
-         * BLUE TARGET
-         * Position:
-         * Image: chips
-         */
-        // set blue target
-        VuforiaTrackable blueTarget  = stonesAndChips.get(1);
-        trackablesList.put(TrackableItem.BLUE_TARGET, blueTarget);
 
-        blueTarget.setName("BlueTarget");  // Chips
+		/**
+		 * now we have to position the images
+		 * they're initially at the center of the field facing up
+		 * we need to move them according to their actual positions
+		 * need to rotate x/y/z and actually postiion the location
+		 * x points to right, y points to top of image, z points out of image towards the other side of the field
+		 */
 
-        // also wrong, we gotta set it manually
-        OpenGLMatrix blueTargetLocationOnField = OpenGLMatrix
-                /* Then we translate the target off to the Blue Audience wall.
-                Our translation here is a positive translation in Y.*/
-                .translation(0, mmFTCFieldWidth/2, 0)
-                .multiplied(Orientation.getRotationMatrix(
-                        /* First, in the fixed (field) coordinate system, we rotate 90deg in X */
-                        AxesReference.EXTRINSIC, AxesOrder.XZX,
-                        AngleUnit.DEGREES, 90, 0, 0));
-        blueTarget.setLocation(blueTargetLocationOnField);
-        RobotLog.ii(TAG, "Blue Target=%s", format(blueTargetLocationOnField));
+		redAllianceTarget.setLocation(OpenGLMatrix
+				.translation(0, -halfField, mmTargetHeight)
+				.multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 180)));
 
-        // let blue target listener know where phone is
-        ((VuforiaTrackableDefaultListener)blueTarget.getListener()).setPhoneInformation(phoneLocationOnRobot, cameraDirection);
+		blueAllianceTarget.setLocation(OpenGLMatrix
+				.translation(0, halfField, mmTargetHeight)
+				.multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, 0)));
+		frontWallTarget.setLocation(OpenGLMatrix
+				.translation(-halfField, 0, mmTargetHeight)
+				.multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0 , 90)));
+
+		// The tower goal targets are located a quarter field length from the ends of the back perimeter wall.
+		blueTowerGoalTarget.setLocation(OpenGLMatrix
+				.translation(halfField, quadField, mmTargetHeight)
+				.multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0 , -90)));
+		redTowerGoalTarget.setLocation(OpenGLMatrix
+				.translation(halfField, -quadField, mmTargetHeight)
+				.multiplied(Orientation.getRotationMatrix(EXTRINSIC, XYZ, DEGREES, 90, 0, -90)));
     }
 
-    /**
-     * Sets the position of the phone on the robot
-     * SEE THE GITHUB FOR HOW TO DO THIS, ITS REALLY DETAILED
-     * GO READ IT.
-     * https://github.com/FIRST-Tech-Challenge/FtcRobotController/blob/master/FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/external/samples/ConceptVuforiaNavigation.java
-     */
-    public void setPhoneLocation() {
-        phoneLocationOnRobot = OpenGLMatrix
-                .translation(mmBotWidth/2,0,0)
-                .multiplied(Orientation.getRotationMatrix(
-                        AxesReference.EXTRINSIC, AxesOrder.YZY,
-                        AngleUnit.DEGREES, -90, 0, 0));
+	public void setPhoneLocation() {
+		// rotate camera so that it faces forward
+		phoneYRotate = -90;
 
-        RobotLog.ii(TAG, "phone=%s", format(phoneLocationOnRobot));
-    }
+		// transformation matrix that describes where camera is on robot
+		// initially the webcam is assumed to face up and the top points to the left of the robot
+
+		// the top of the webcam is just the highest part when it's mounted correctly
+
+		// measure these out and change them to be applicable to the current robot
+		final float CAMERA_FORWARD_DISPLACEMENT  = 200.0f;   // eg: Camera is ___ mm in front of robot-center
+		final float CAMERA_VERTICAL_DISPLACEMENT = 160.0f;   // eg: Camera is ___ mm above ground
+		final float CAMERA_LEFT_DISPLACEMENT = 155.0f;     // eg: Camera is __ mm to the right of the robot's center line
+
+		OpenGLMatrix robotFromCamera = OpenGLMatrix
+					.translation(CAMERA_FORWARD_DISPLACEMENT, CAMERA_LEFT_DISPLACEMENT, CAMERA_VERTICAL_DISPLACEMENT)
+					.multiplied(Orientation.getRotationMatrix(EXTRINSIC, YZX, DEGREES, phoneYRotate, phoneZRotate, phoneXRotate));
+
+		// tell trackables (pictures) the camera position
+		for (VuforiaTrackable trackable : allTrackables) {
+			((VuforiaTrackableDefaultListener) trackable.getListener()).setPhoneInformation(robotFromCamera, parameters.cameraDirection);
+		}
+	}
+
 
     /** END INITIALIZATIION */
 
-
-    /**
-     * Extracts positioning info from a transformation matrix and returns it as a readable string
-     * Taken from the github
-     * @param transformationMatrix The matrix to get info from
-     * @return the information as a readable string
-     */
-    public static String format(OpenGLMatrix transformationMatrix) {
-        return (transformationMatrix != null) ? transformationMatrix.formatAsTransform() : "null";
-    }
+	/**
+	 * A simple utility that extracts positioning information from a transformation matrix
+	 * and formats it in a form palatable to a human being.
+	 */
+	String format(OpenGLMatrix transformationMatrix) {
+		return transformationMatrix.formatAsTransform();
+	}
 
     // get location
     // check if target is visible
@@ -210,7 +222,28 @@ public class Vuforia {
         return lastLocation;
     }
 
-
-
-
+		// method to convert what the webcam is seeing into a .png, 
+	void captureFrameToFile() {
+		vuforia.getFrameOnce(Continuation.create(ThreadPool.getDefault(), new Consumer<Frame>()
+			{
+			@Override public void accept(Frame frame)
+				{
+				Bitmap bitmap = vuforia.convertFrameToBitmap(frame);
+				if (bitmap != null) {
+					File file = new File(captureDirectory, String.format(Locale.getDefault(), "VuforiaFrame-%d.png", captureCounter++));
+					try {
+						FileOutputStream outputStream = new FileOutputStream(file);
+						try {
+							bitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream);
+						} finally {
+							outputStream.close();
+							telemetry.log().add("captured %s", file.getName());
+						}
+					} catch (IOException e) {
+						RobotLog.ee(TAG, e, "exception in captureFrameToFile()");
+					}
+				}
+			}
+		}));
+	}
 }
